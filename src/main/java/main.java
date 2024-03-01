236b769d341ba73f42e1828822778e4d73d07fe5
highcera <highcera@gmail.com>
2024-03-02 08:42:44 +0900

    생성된 게시물 정보를 Article 클래스의 객체로 저장 후 문장으로 출력

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -22,17 +22,28 @@ public class main {
                 String body = sc.nextLine();
 
                 int id = ++articleLastId;
-                System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+
+                Article article = new Article();
+                article.id = id;
+                article.title = title;
+                article.body = body;
+
+                System.out.println("생성된 게시물 객체 : " + article);
+                System.out.printf("%d번 게시물이 생성되었습니다.\n", article.id);
 
             } else if(cmd.equals("exit")) {
                 System.out.printf("입력받은 명령어 : %s\n", cmd);
                 break;
             }
-
-//            System.out.printf("입력받은 명령어 : %s\n", cmd);
         }
 
         System.out.println("== 자바 텍스트 게시판 종료 ==");
         sc.close();
     }
+}
+
+class Article {
+    int id;
+    String title;
+    String body;
 }
