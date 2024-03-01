8c521f6f77aae04343a049f4eb4d89d68b474081
highcera <highcera@gmail.com>
2024-03-02 08:03:21 +0900

    exit를 압력받기 전까지 계속 입력

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -7,12 +7,19 @@ public class main {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.printf("명령)");
-        String cmd = sc.nextLine();
+        while (true) {
+            System.out.printf("명령)");
+            String cmd = sc.nextLine();
 
-        System.out.printf("입력받은 명령어 : %s\n", cmd);
+            if(cmd.equals("exit")) {
+                System.out.printf("입력받은 명령어 : %s\n", cmd);
+                break;
+            }
+
+            System.out.printf("입력받은 명령어 : %s\n", cmd);
+        }
+
         System.out.println("== 자바 텍스트 게시판 종료 ==");
-
         sc.close();
     }
 }
