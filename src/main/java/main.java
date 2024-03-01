9b3ff82042841ba62bd91580c354ea9fda2a1138
highcera <highcera@gmail.com>
2024-03-01 20:17:13 +0900

    시작 알림 메시지 출력

---
+++ src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("== 자바 텍스트 게시판 0.1v ==");
+        System.out.println("== 자바 텍스트 게시판 시작 ==");
+        System.out.println("== 자바 텍스트 게시판 종료 ==");
+    }
+}
