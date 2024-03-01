04fecd58a9672d6fd90a32821f8d91442f902ddd
highcera <highcera@gmail.com>
2024-03-02 08:14:51 +0900

    usr/article/write라고 입력하면 게시물 제목, 내용 입력받기

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -11,12 +11,23 @@ public class main {
             System.out.printf("명령)");
             String cmd = sc.nextLine();
 
-            if(cmd.equals("exit")) {
+            if (cmd.equals("/usr/article/write")) {
+                System.out.println("== 게시물 작성 ==");
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+
+                int id = 1;
+                System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
+
+            } else if(cmd.equals("exit")) {
                 System.out.printf("입력받은 명령어 : %s\n", cmd);
                 break;
             }
 
-            System.out.printf("입력받은 명령어 : %s\n", cmd);
+//            System.out.printf("입력받은 명령어 : %s\n", cmd);
         }
 
         System.out.println("== 자바 텍스트 게시판 종료 ==");
